d66a59d41304bc141e560ba1a4d1f5ab530cae32
Andrew Vittiglio <andrew@Andrews-MacBook-Air.local>
2022-07-03 01:15:05 -0400

    added setInterval for continuous upload checking

--- run-api.js
+++ run-api.js
@@ -1,33 +1,24 @@
 const config = require('./src/models/config');
 const frames = require('./src/YTtoFrames');
 
-
-for(var i=0; i<1;i++){
-	var url = require('./src/urlFetcher');
-
+setInterval(function(){
+	var url = require('./src/urlFetcher'); //checks if new video
 
 	//everything MUST be done within the setTimeout() because I suck at async :(
 	setTimeout(function(){
-		
-			
 			if(!config.actionStatus){
-				console.log("KSI has not uploaded a new video");
+				console.log("No New Upload");
 			}
-			else{
-			//if the url has changed (i.e. new video uploaded), go to step #2
-			console.log("New Video Uploaded!");
-			let options = {videoName: 'video', fps: 0.5, imgFileName: "img", downloadLocation: './src/models/images'};
-			frames(config.newVideoURL, options);
-
-			
-
-
-
-			return;
+			else{	
+				//if the url has changed (i.e. new video uploaded), go to step #2
+				console.log("New Video Uploaded!");
+				let options = {videoName: 'video', fps: 1, imgFileName: "img", downloadLocation: './src/models/images'};
+				frames(config.newVideoURL, options)
+				return;
 			}
 	},2000);
-}
 
+},5000);
 /*
 to delete batch of images: 
 find . -type f -name "img*" -print -delete && rm video.mp4
